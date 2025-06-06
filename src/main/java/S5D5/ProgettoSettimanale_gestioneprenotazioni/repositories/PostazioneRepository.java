7743cb9b8614062dee88250d8040e692ba715d8c
AngeloZorzi <astro.drg666@gmail.com>
2025-06-06 12:07:52 +0200

    upd

--- src/main/java/S5D5/ProgettoSettimanale_gestioneprenotazioni/repositories/PostazioneRepository.java
+++ src/main/java/S5D5/ProgettoSettimanale_gestioneprenotazioni/repositories/PostazioneRepository.java
@@ -1,4 +1,7 @@
 package S5D5.ProgettoSettimanale_gestioneprenotazioni.repositories;
 
-public interface PostazioneRepository {
+import S5D5.ProgettoSettimanale_gestioneprenotazioni.entities.Postazione;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostazioneRepository extends JpaRepository<Postazione, Long> {
 }
